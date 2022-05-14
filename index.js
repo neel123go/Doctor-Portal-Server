cdf7246f967f0dcda1c40c7e954585fbf5aca435
neel123go <ayounpaul1029@gmail.com>
2022-05-14 16:27:58 +0600

    created api for get all treatment data form database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,28 @@ const port = process.env.PORT || 5000;
 // Use Middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pvyew.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const treatmentCollection = client.db('Doctor_Portals').collection('treatments');
+
+        app.get('/treatment', async (req, res) => {
+            const query = {};
+            const cursor = treatmentCollection.find(query);
+            const treatment = await cursor.toArray();
+            res.send(treatment);
+        });
+
+    } finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello for doctor uncle');
